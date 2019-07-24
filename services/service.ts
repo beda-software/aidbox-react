eadc3e6785e8986e6fa3a1c6553cd76ca1e9afb2
Vadim Laletin <rusmiligamer@gmail.com>
2019-07-24 17:20:25 +0700

    Add data/error transformers for services

--- services/service.ts
+++ services/service.ts
@@ -1,6 +1,13 @@
 import { AxiosRequestConfig } from 'axios';
 
-import { RemoteDataResult, failure, success } from '../libs/remoteData';
+import {
+    RemoteDataResult,
+    failure,
+    success,
+    isSuccess,
+    RemoteData,
+    isFailure
+} from '../libs/remoteData';
 import { axiosInstance } from './instance';
 
 export async function service<S = any, F = any>(config: AxiosRequestConfig): Promise<RemoteDataResult<S, F>> {
@@ -11,4 +18,30 @@ export async function service<S = any, F = any>(config: AxiosRequestConfig): Pro
     } catch (err) {
         return failure(err.response ? err.response.data : err.message);
     }
+}
+
+export async function applyDataTransformer<S = any, F = any, R = any>(
+    servicePromise: Promise<RemoteData<S, F>>,
+    transformer: (data: S) => R
+): Promise<RemoteData<R, F>> {
+    const response = await servicePromise;
+
+    if (isSuccess(response)) {
+        return success(transformer(response.data));
+    }
+
+    return response;
+}
+
+export async function applyErrorTransformer<S = any, F = any, R = any>(
+    servicePromise: Promise<RemoteData<S, F>>,
+    transformer: (error: F) => R
+): Promise<RemoteData<S, R>> {
+    const response = await servicePromise;
+
+    if (isFailure(response)) {
+        return failure(transformer(response.error));
+    }
+
+    return response;
 }
