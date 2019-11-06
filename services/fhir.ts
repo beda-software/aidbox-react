c993b088e1e3540e3e4a100f1569bc8821ac7819
Ilya Beda <ir4y.ix@gmail.com>
2019-11-06 12:43:51 +0300

    Add forceDeleteFHIRResource function

--- services/fhir.ts
+++ services/fhir.ts
@@ -183,6 +183,17 @@ export async function deleteFHIRResource<R extends AidboxResource>(
     });
 }
 
+export async function forceDeleteFHIRResource<R extends AidboxResource>(
+    resource: AidboxReference<R>,
+    params?: SearchParams,
+): Promise<RemoteDataResult<R>> {
+    return service({
+        method: 'DELETE',
+        url: `/${resource.resourceType}/${resource.id}`,
+        params: params || {},
+    });
+}
+
 export function getReference<T extends AidboxResource>(resource: T, display?: string): AidboxReference<T> {
     return {
         resourceType: resource.resourceType,
