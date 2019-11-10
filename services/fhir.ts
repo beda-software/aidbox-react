85c8e3839b3e18913e10512f7afab610ab4c7f86
alex <pavlushkin87@gmail.com>
2019-11-10 14:08:51 +0700

    Change type to any

--- services/fhir.ts
+++ services/fhir.ts
@@ -1,256 +1,285 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-import { AidboxReference, AidboxResource, Bundle, ValueSet } from 'src/contrib/aidbox';
+import {
+  AidboxReference,
+  AidboxResource,
+  Bundle,
+  ValueSet
+} from "src/contrib/aidbox";
 
-import { failure, RemoteDataResult } from '../libs/remoteData';
-import { service } from './service';
-import { SearchParams } from './search';
+import { failure, RemoteDataResult } from "../libs/remoteData";
+import { service } from "./service";
+import { SearchParams } from "./search";
 
 interface InactiveMappingItem {
-    searchField: string;
-    statusField: string;
-    value: any;
+  searchField: string;
+  statusField: string;
+  value: any;
 }
 
 interface InactiveMapping {
-    [resourceType: string]: InactiveMappingItem;
+  [resourceType: string]: InactiveMappingItem;
 }
 
 const inactiveMapping: InactiveMapping = {
-    DocumentReference: {
-        searchField: 'status',
-        statusField: 'status',
-        value: 'entered-in-error',
-    },
-    Observation: {
-        searchField: 'status',
-        statusField: 'status',
-        value: 'entered-in-error',
-    },
-    Location: {
-        searchField: 'status',
-        statusField: 'status',
-        value: 'inactive',
-    },
-    Schedule: {
-        searchField: 'active',
-        statusField: 'active',
-        value: false,
-    },
-    Slot: {
-        searchField: 'status',
-        statusField: 'status',
-        value: 'entered-in-error',
-    },
-    Practitioner: {
-        searchField: 'active',
-        statusField: 'active',
-        value: false,
-    },
-    Patient: {
-        searchField: 'active',
-        statusField: 'active',
-        value: false,
-    },
-    User: {
-        searchField: 'active',
-        statusField: 'active',
-        value: false,
-    },
-    Note: {
-        searchField: 'status',
-        statusField: 'status',
-        value: 'entered-in-error',
-    },
-    EpisodeOfCare: {
-        searchField: 'status',
-        statusField: 'status',
-        value: 'entered-in-error',
-    },
+  DocumentReference: {
+    searchField: "status",
+    statusField: "status",
+    value: "entered-in-error"
+  },
+  Observation: {
+    searchField: "status",
+    statusField: "status",
+    value: "entered-in-error"
+  },
+  Location: {
+    searchField: "status",
+    statusField: "status",
+    value: "inactive"
+  },
+  Schedule: {
+    searchField: "active",
+    statusField: "active",
+    value: false
+  },
+  Slot: {
+    searchField: "status",
+    statusField: "status",
+    value: "entered-in-error"
+  },
+  Practitioner: {
+    searchField: "active",
+    statusField: "active",
+    value: false
+  },
+  Patient: {
+    searchField: "active",
+    statusField: "active",
+    value: false
+  },
+  User: {
+    searchField: "active",
+    statusField: "active",
+    value: false
+  },
+  Note: {
+    searchField: "status",
+    statusField: "status",
+    value: "entered-in-error"
+  },
+  EpisodeOfCare: {
+    searchField: "status",
+    statusField: "status",
+    value: "entered-in-error"
+  }
 };
 
 function getInactiveSearchParam(resourceType: string) {
-    const item = inactiveMapping[resourceType];
+  const item = inactiveMapping[resourceType];
 
-    if (item) {
-        return {
-            [`${item.searchField}:not`]: [item.value],
-        };
-    }
+  if (item) {
+    return {
+      [`${item.searchField}:not`]: [item.value]
+    };
+  }
 
-    return {};
+  return {};
 }
 
 export async function getFHIRResource<R extends AidboxResource>(
-    reference: AidboxReference<R>
+  reference: AidboxReference<R>
 ): Promise<RemoteDataResult<R>> {
-    return service({
-        method: 'GET',
-        url: `/${reference.resourceType}/${reference.id}`,
-    });
+  return service({
+    method: "GET",
+    url: `/${reference.resourceType}/${reference.id}`
+  });
 }
 
 export async function getFHIRResources<R extends AidboxResource>(
-    resourceType: R['resourceType'],
-    params: SearchParams,
-    extraPath?: string
+  resourceType: R["resourceType"],
+  params: SearchParams,
+  extraPath?: string
 ): Promise<RemoteDataResult<Bundle<R>>> {
-    return service({
-        method: 'GET',
-        url: extraPath ? `/${resourceType}/${extraPath}` : `/${resourceType}`,
-        params: { ...params, ...getInactiveSearchParam(resourceType) },
-    });
+  return service({
+    method: "GET",
+    url: extraPath ? `/${resourceType}/${extraPath}` : `/${resourceType}`,
+    params: { ...params, ...getInactiveSearchParam(resourceType) }
+  });
 }
 
 export async function findFHIRResource<R extends AidboxResource>(
-    resourceType: R['resourceType'],
-    params: SearchParams,
-    extraPath?: string
+  resourceType: R["resourceType"],
+  params: SearchParams,
+  extraPath?: string
 ): Promise<RemoteDataResult<R>> {
-    return await service({
-        method: 'GET',
-        url: extraPath ? `/${resourceType}/${extraPath}` : `/${resourceType}`,
-        params: { ...params, ...getInactiveSearchParam(resourceType) },
-        transformResponse: (resp: string) => {
-            const data: Bundle<R> = JSON.parse(resp);
-            const resources = data.entry!;
-            if (resources.length === 1) {
-                return resources[0].resource!;
-            } else if (resources.length === 0) {
-                throw 'No resources found';
-            } else {
-                throw 'Too many resources found';
-            }
-        },
-    });
+  return await service({
+    method: "GET",
+    url: extraPath ? `/${resourceType}/${extraPath}` : `/${resourceType}`,
+    params: { ...params, ...getInactiveSearchParam(resourceType) },
+    transformResponse: (resp: string) => {
+      const data: Bundle<R> = JSON.parse(resp);
+      const resources = data.entry!;
+      if (resources.length === 1) {
+        return resources[0].resource!;
+      } else if (resources.length === 0) {
+        throw "No resources found";
+      } else {
+        throw "Too many resources found";
+      }
+    }
+  });
 }
 
-export async function saveFHIRResource<R extends AidboxResource>(resource: R): Promise<RemoteDataResult<R>> {
-    return service({
-        method: resource.id ? 'PUT' : 'POST',
-        data: resource,
-        url: `/${resource.resourceType}${resource.id ? '/' + resource.id : ''}`,
-    });
+export async function saveFHIRResource<R extends AidboxResource>(
+  resource: R
+): Promise<RemoteDataResult<R>> {
+  return service({
+    method: resource.id ? "PUT" : "POST",
+    data: resource,
+    url: `/${resource.resourceType}${resource.id ? "/" + resource.id : ""}`
+  });
 }
 
 export async function saveFHIRResources<R extends AidboxResource>(
-    resources: R[],
-    bundleType: 'transaction' | 'batch'
+  resources: R[],
+  bundleType: "transaction" | "batch"
 ): Promise<RemoteDataResult<Bundle<R>>> {
-    return service({
-        method: 'POST',
-        url: '/',
-        data: {
-            type: bundleType,
-            entry: _.map(resources, (resource) => ({
-                resource,
-                request: {
-                    method: resource.id ? 'PUT' : 'POST',
-                    url: `/${resource.resourceType}${resource.id ? '/' + resource.id : ''}`,
-                },
-            })),
-        },
-    });
+  return service({
+    method: "POST",
+    url: "/",
+    data: {
+      type: bundleType,
+      entry: _.map(resources, resource => ({
+        resource,
+        request: {
+          method: resource.id ? "PUT" : "POST",
+          url: `/${resource.resourceType}${
+            resource.id ? "/" + resource.id : ""
+          }`
+        }
+      }))
+    }
+  });
 }
 
 export async function patchFHIRResource<R extends AidboxResource>(
-    resource: Partial<R> & Required<Pick<R, 'id' | 'resourceType'>>
+  resource: Partial<R> & Required<Pick<R, "id" | "resourceType">>
 ): Promise<RemoteDataResult<R>> {
-    return service({
-        method: 'PATCH',
-        data: resource,
-        url: `/${resource.resourceType}/${resource.id}`,
-    });
+  return service({
+    method: "PATCH",
+    data: resource,
+    url: `/${resource.resourceType}/${resource.id}`
+  });
 }
 
 export async function deleteFHIRResource<R extends AidboxResource>(
-    resource: AidboxReference<R>
+  resource: AidboxReference<R>
 ): Promise<RemoteDataResult<R>> {
-    const inactiveMappingItem = inactiveMapping[resource.resourceType];
+  const inactiveMappingItem = inactiveMapping[resource.resourceType];
 
-    if (!inactiveMappingItem) {
-        console.error(`Please specify inactiveMapping for ${resource.resourceType} to mark item deleted`);
+  if (!inactiveMappingItem) {
+    console.error(
+      `Please specify inactiveMapping for ${resource.resourceType} to mark item deleted`
+    );
 
-        return failure({});
+    return failure({});
+  }
+
+  return service({
+    method: "PATCH",
+    url: `/${resource.resourceType}/${resource.id}`,
+    data: {
+      [inactiveMappingItem.statusField]: inactiveMappingItem.value
     }
-
-    return service({
-        method: 'PATCH',
-        url: `/${resource.resourceType}/${resource.id}`,
-        data: {
-            [inactiveMappingItem.statusField]: inactiveMappingItem.value,
-        },
-    });
+  });
 }
 
 export async function forceDeleteFHIRResource<R extends AidboxResource>(
-    resource: AidboxReference<R>,
-    params?: SearchParams,
+  resource: AidboxReference<R>,
+  params?: SearchParams
 ): Promise<RemoteDataResult<R>> {
-    return service({
-        method: 'DELETE',
-        url: `/${resource.resourceType}/${resource.id}`,
-        params: params || {},
-    });
+  return service({
+    method: "DELETE",
+    url: `/${resource.resourceType}/${resource.id}`,
+    params: params || {}
+  });
 }
 
-export function getReference<T extends AidboxResource>(resource: T, display?: string): AidboxReference<T> {
-    return {
-        resourceType: resource.resourceType,
-        id: resource.id!,
-        ...(display ? { display } : {}),
-    };
+export function getReference<T extends AidboxResource>(
+  resource: T,
+  display?: string
+): AidboxReference<T> {
+  return {
+    resourceType: resource.resourceType,
+    id: resource.id!,
+    ...(display ? { display } : {})
+  };
 }
 
 export function makeReference<T extends AidboxResource>(
-    resourceType: string,
-    id: string,
-    display?: string
+  resourceType: string,
+  id: string,
+  display?: string
 ): AidboxReference<T> {
-    return {
-        resourceType,
-        id,
-        display,
-    };
+  return {
+    resourceType,
+    id,
+    display
+  };
 }
 
 export function isReference<T extends AidboxResource>(
-    resource: T | AidboxReference<T>
+  resource: T | AidboxReference<T>
 ): resource is AidboxReference<T> {
-    return _.isEmpty(
-        _.difference(_.keys(resource), [
-            'id',
-            'resourceType',
-            '_id',
-            'resource',
-            'display',
-            'identifier',
-            'uri',
-            'localRef',
-            'extension',
-        ])
-    );
+  return _.isEmpty(
+    _.difference(_.keys(resource), [
+      "id",
+      "resourceType",
+      "_id",
+      "resource",
+      "display",
+      "identifier",
+      "uri",
+      "localRef",
+      "extension"
+    ])
+  );
 }
 
-export type ResourcesMap<T extends AidboxResource> = { [P in T['resourceType']]: T[] | undefined };
-export function extractBundleResources<T extends AidboxResource>(bundle: Bundle<T>): ResourcesMap<T> {
-    const entriesByResourceType = _.groupBy(bundle.entry, (entry) => entry.resource!.resourceType);
+export type ResourcesMap<T extends AidboxResource> = {
+  [P in T["resourceType"]]: T[] | undefined;
+};
+export function extractBundleResources<T extends AidboxResource>(
+  bundle: Bundle<T>
+): any {
+  const entriesByResourceType = _.groupBy(
+    bundle.entry,
+    entry => entry.resource!.resourceType
+  );
 
-    return _.mapValues(entriesByResourceType, (entries) => _.map(entries, (entry) => entry.resource!));
+  return _.mapValues(entriesByResourceType, entries =>
+    _.map(entries, entry => entry.resource!)
+  );
 }
 
 export function getIncludedResource<T extends AidboxResource>(
-    // TODO: improve type for includedResources: must contain T
-    resources: ResourcesMap<T | any>,
-    reference: AidboxReference<T>
+  // TODO: improve type for includedResources: must contain T
+  resources: ResourcesMap<T | any>,
+  reference: AidboxReference<T>
 ) {
-    return _.find<T>(resources[reference.resourceType], (resource) => resource.id === reference.id);
+  return _.find<T>(
+    resources[reference.resourceType],
+    resource => resource.id === reference.id
+  );
 }
 
-export function getConcepts(valueSetId: string, params?: SearchParams): Promise<RemoteDataResult<ValueSet>> {
-    return service({
-        method: 'GET',
-        url: `/ValueSet/${valueSetId}/$expand`,
-        params: { ...params },
-    });
+export function getConcepts(
+  valueSetId: string,
+  params?: SearchParams
+): Promise<RemoteDataResult<ValueSet>> {
+  return service({
+    method: "GET",
+    url: `/ValueSet/${valueSetId}/$expand`,
+    params: { ...params }
+  });
 }
