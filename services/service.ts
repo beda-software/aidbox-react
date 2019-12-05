4000ce93bb8117ea412f50b1cb391e0323d5fc1e
Vadim Laletin <rusmiligamer@gmail.com>
2019-12-05 18:47:57 +0700

    Add resolveServiceMap helper

--- services/service.ts
+++ services/service.ts
@@ -1,12 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
-
-import {
-    failure,
-    isFailure,
-    isSuccess,
-    RemoteDataResult,
-    success
-} from '../libs/remoteData';
+import _ from 'lodash';
+import { failure, isFailure, isSuccess, isSuccessAll, RemoteDataResult, success } from '../libs/remoteData';
 import { axiosInstance } from './instance';
 
 export async function service<S = any, F = any>(config: AxiosRequestConfig): Promise<RemoteDataResult<S, F>> {
@@ -43,4 +37,22 @@ export async function applyErrorTransformer<S = any, F = any, R = any>(
     }
 
     return response;
+}
+
+type PromiseRemoteDataResultMap<T, F> = { [P in keyof T]: Promise<RemoteDataResult<T[P], F>> };
+export async function resolveServiceMap<I, F>(
+    promisesMap: PromiseRemoteDataResultMap<I, F>
+): Promise<RemoteDataResult<I, F[]>> {
+    const keys = _.keys(promisesMap);
+    const values = _.values(promisesMap);
+
+    const responses = (await Promise.all(values)) as Array<RemoteDataResult<any>>;
+
+    if (isSuccessAll(responses)) {
+        return success(_.zipObject(keys, _.map(responses, (response) => response.data)) as I);
+    } else {
+        return failure(_.compact(
+            _.map(responses, (response) => (isFailure(response) ? response.error : undefined))
+        ));
+    }
 }
