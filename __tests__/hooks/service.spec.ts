fafd81c593d2fc6357a4e7ef64579d56cdaed932
Sergey Ufocoder <xufocoder@gmail.com>
2020-01-19 17:20:08 +0300

    Add test for `hooks/service`

---
+++ __tests__/hooks/service.spec.ts
@@ -0,0 +1,62 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useService } from '../../hooks/service';
+import { success } from '../../libs/remoteData';
+
+
+describe('Hook `useService`', () => {
+    const data = { custom: 'data'}
+    const service = () => Promise.resolve(success(data))
+
+    const remoteDataSuccess = {
+        status: 'Success',
+        data
+    }
+
+    const remoteDataLoading = {
+        status: 'Loading'
+    }
+
+    test('change `Loading` to `Success` status when service resolved', async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useService(service))
+
+        expect(result.current[0]).toEqual(remoteDataLoading)
+
+        await waitForNextUpdate()
+
+        expect(result.current[0]).toEqual(remoteDataSuccess)
+    })
+
+    test('method `reload` returns the same data ', async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useService(service))
+
+        await waitForNextUpdate()
+
+        expect(result.current[0]).toEqual(remoteDataSuccess)
+
+        act(() => {
+            result.current[1].reload()
+        })
+
+        expect(result.current[0]).toEqual(remoteDataLoading)
+
+        await waitForNextUpdate()
+
+        expect(result.current[0]).toEqual(remoteDataSuccess)
+    })
+
+    test('has `set` data method returns success remote data', async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useService(service))
+        const data = { custom: 'data-new' }
+
+        await waitForNextUpdate()
+        
+        act(() => {
+            result.current[1].set(data)
+        })
+
+        expect(result.current[0]).toEqual({
+            status: 'Success',
+            data
+        })
+    })
+})
