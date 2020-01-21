fdbcaa0a040bd992e7c80d659e7e573e92260e4a
Sergey Ufocoder <xufocoder@gmail.com>
2020-01-21 16:20:28 +0300

    Update test for `services/fhir`

--- __tests__/services/fhir.spec.ts
+++ __tests__/services/fhir.spec.ts
@@ -1,12 +1,17 @@
 import { 
     getFHIRResource, 
     getFHIRResources,
+    findFHIRResource,
+    saveFHIRResource,
+    saveFHIRResources,
     patchFHIRResource,
     forceDeleteFHIRResource,
     deleteFHIRResource,
     getReference,
     makeReference,
     isReference,
+    getIncludedResource,
+    getIncludedResources,
     getConcepts
 } from '../../services/fhir';
 
@@ -17,7 +22,6 @@ import { success, failure } from '../../libs/remoteData';
 jest.mock('../../services/service', () => {
     return { service: jest.fn(() => Promise.resolve(success('data'))) }
 })
-
 
 describe('Service `fhir`', () => {
 
@@ -59,6 +63,112 @@ describe('Service `fhir`', () => {
         })
     })
 
+    test('method `saveFHIRResource` 1', async () => {
+        const resourceWithId = { id: '1', resourceType: 'type' }
+        const resourceWithoutId = { resourceType: 'type' }
+
+        saveFHIRResource(resourceWithId)
+
+        expect((<jest.Mock>service).mock.calls[0][0]).toEqual({
+            method: 'PUT', 
+            url: '/type/1', 
+            data: resourceWithId
+        })
+
+        await saveFHIRResource(resourceWithoutId)
+
+        expect((<jest.Mock>service).mock.calls[1][0]).toEqual({
+            method: 'POST', 
+            url: '/type', 
+            data: resourceWithoutId
+        })
+    })
+
+    test('method `saveFHIRResources`', async () => {
+        const bundleType = 'transaction'
+        const resources = [
+            { id: '1', resourceType: 'type' },
+            { id: '2', resourceType: 'type' },
+            { resourceType: 'type' }
+        ]
+
+        await saveFHIRResources(resources, bundleType)
+
+        expect((<jest.Mock>service).mock.calls[0][0]).toEqual({
+            method: 'POST', 
+            url: '/', 
+            data: {
+                type: bundleType,
+                entry: [
+                    {
+                        request: {
+                            method: "PUT",
+                            url: "/type/1",
+                        },
+                        resource: {
+                            id: '1', 
+                            resourceType: 'type'
+                        }
+                    },
+                    {
+                        request: {
+                            method: "PUT",
+                            url: "/type/2",
+                        },
+                        resource: {
+                            id: '2', 
+                            resourceType: 'type'
+                        }
+                    },
+                    {
+                        request: {
+                            method: "POST",
+                            url: "/type",
+                        },
+                        resource: {
+                            resourceType: 'type'
+                        }
+                    }
+                ]
+            }
+        })
+    })
+    
+    test('method `findFHIRResource`', async () => {
+        const params = { id: 1 }
+        const resourceType = 'type'
+
+        await findFHIRResource(resourceType, params)
+
+        const config = (<jest.Mock>service).mock.calls[0][0]
+
+        expect(config).toEqual(expect.objectContaining({
+            method: 'GET',
+            url: '/' + resourceType,
+            params
+        }))
+
+        expect(() => {
+            const response = ''
+            config.transformResponse(response)
+         }).toThrow();
+
+        expect(() => {
+            const response = '{"entry":[]}'
+            config.transformResponse(response)
+         }).toThrow();
+
+        const response = '{"entry":[{"resource": "data"}]}'
+        const transformed = config.transformResponse(response)
+
+        expect(transformed).toBe('data')
+
+        expect(() => {
+            const response = '{"entry":[{"resource": "a"}, {"resource": "b"}]}'
+            config.transformResponse(response)
+         }).toThrow();
+    })
+
     test('method `patchFHIRResource`', async () => {
         const resource = { 
             id: '1',
@@ -73,10 +183,8 @@ describe('Service `fhir`', () => {
             url: `/${resource.resourceType}/${resource.id}`,
         })
     })
-    
-    test('method `deleteFHIRResource`', async () => {
-        
 
+    test('method `deleteFHIRResource`', async () => {
         const result = await deleteFHIRResource({ 
             id: '1',
             resourceType: 'Unknown'
@@ -151,6 +259,30 @@ describe('Service `fhir`', () => {
             resourceType: 'type',
             extraField: true
         })).toBeFalsy()  
+    })
+
+    test('method `getIncludedResource` 1', () => {
+        const resources = {
+            customType: [
+                { id: '1' }
+            ],
+        }
+
+        const referenceFirst = { id: '1', resourceType: 'customType' }
+        const referenceSecond = { id: '2', resourceType: 'customType' }
+
+        expect(getIncludedResource(resources, referenceFirst)).toBeTruthy()
+        expect(getIncludedResource(resources, referenceSecond)).toBeFalsy()
+    })
+
+    test('method `getIncludedResources`', () => {
+        const customTypeResources = [1, 2, 3]
+        const resourceType = 'customType'
+        const resources = {
+            customType: customTypeResources
+        }
+
+        expect(getIncludedResources(resources, resourceType)).toEqual(customTypeResources)
     })
 
     test('method `getConcepts`', async () => {
