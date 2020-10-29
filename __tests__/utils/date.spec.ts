858965bce211bf99546597acb5c645788920280a
Sergey Ufocoder <xufocoder@gmail.com>
2020-10-30 00:33:16 +0300

    Add `date` utils test

---
+++ __tests__/utils/date.spec.ts
@@ -0,0 +1,52 @@
+import {
+    // formatFHIRTime,
+    // formatFHIRDate,
+    // formatFHIRDateTime,
+    FHIRDateFormat,
+    FHIRTimeFormat,
+    FHIRDateTimeFormat,
+    parseFHIRTime,
+    parseFHIRDate,
+    parseFHIRDateTime,
+    extractFHIRDate,
+    extractFHIRTime,
+} from '../../utils/date';
+
+describe('Util `tests`', () => {
+    describe(`parse and format`, () => {
+        test('time', () => {
+            expect(parseFHIRTime('12').format(FHIRTimeFormat)).toEqual('12:00:00');
+            expect(parseFHIRTime('13:30').format(FHIRTimeFormat)).toEqual('13:30:00');
+            expect(parseFHIRTime('21:30:15').format(FHIRTimeFormat)).toEqual('21:30:15');
+        });
+
+        test('date', () => {
+            expect(parseFHIRDate('2020').format(FHIRDateFormat)).toEqual('2020-01-01');
+            expect(parseFHIRDate('2020-10').format(FHIRDateFormat)).toEqual('2020-10-01');
+            expect(parseFHIRDate('2020-10-05').format(FHIRDateFormat)).toEqual('2020-10-05');
+        });
+
+        test('datetime', () => {
+            expect(parseFHIRDateTime('2020').format(FHIRDateTimeFormat)).toEqual('2020-01-01T03:00:00Z');
+            expect(parseFHIRDateTime('2020-10').format(FHIRDateTimeFormat)).toEqual('2020-10-01T03:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05').format(FHIRDateTimeFormat)).toEqual('2020-10-05T03:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05 12').format(FHIRDateTimeFormat)).toEqual('2020-10-05T15:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05 13:30').format(FHIRDateTimeFormat)).toEqual('2020-10-05T16:30:00Z');
+            expect(parseFHIRDateTime('2020-10-05 20:00:00').format(FHIRDateTimeFormat)).toEqual('2020-10-05T23:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05 20:00:00').format(FHIRDateTimeFormat)).toEqual('2020-10-05T23:00:00Z');
+        });
+    });
+
+    describe(`extract`, () => {
+        test('method `extractFHIRDate`', () => {
+            expect(extractFHIRDate('2020')).toEqual('2020-01-01');
+            expect(extractFHIRDate('2020-10')).toEqual('2020-10-01');
+            expect(extractFHIRDate('2020-10-10')).toEqual('2020-10-10');
+            expect(extractFHIRDate('9999-99-99')).toEqual('9999-99-99');
+        });
+
+        test('method `extractFHIRTime`', () => {
+            expect(extractFHIRTime('15:10:10')).toEqual('15:10:10');
+        });
+    });
+});
