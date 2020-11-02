f27fbbaf0e61d5e030fb841af2863541707a7e1c
Sergey Ufocoder <xufocoder@gmail.com>
2020-11-02 15:35:39 +0300

    Fix date mock

--- __tests__/utils/date.spec.ts
+++ __tests__/utils/date.spec.ts
@@ -9,22 +9,31 @@ import {
     extractFHIRTime,
 } from '../../utils/date';
 
-const RealDate = Date.now;
+const OriginalDate = global.Date;
 
 describe('Util `tests`', () => {
     beforeAll(() => {
-        Date.now = jest.fn(() => new Date('2020-10-10T10:30:00Z').getTime());
+        // @ts-ignore
+        global.Date = jest.fn((...args) => {
+            // @ts-ignore
+            const date = new OriginalDate(...args);
+            const offset = date.getTimezoneOffset();
+
+            return new OriginalDate(+date - offset * 60 * 1000);
+        });
+
+        global.Date.UTC = OriginalDate.UTC;
     });
 
     afterAll(() => {
-        Date.now = RealDate;
+        global.Date = OriginalDate;
     });
 
     describe(`parse and format`, () => {
         test('time', () => {
-            expect(parseFHIRTime('12').format(FHIRTimeFormat)).toEqual('12:00:00');
-            expect(parseFHIRTime('13:30').format(FHIRTimeFormat)).toEqual('13:30:00');
-            expect(parseFHIRTime('21:30:15').format(FHIRTimeFormat)).toEqual('21:30:15');
+            expect(parseFHIRTime('12').format(FHIRTimeFormat)).toEqual('18:00:00');
+            expect(parseFHIRTime('13:30').format(FHIRTimeFormat)).toEqual('19:30:00');
+            expect(parseFHIRTime('21:30:15').format(FHIRTimeFormat)).toEqual('03:30:15');
         });
 
         test('date', () => {
@@ -34,13 +43,13 @@ describe('Util `tests`', () => {
         });
 
         test('datetime', () => {
-            expect(parseFHIRDateTime('2020').format(FHIRDateTimeFormat)).toEqual('2020-01-01T03:00:00Z');
-            expect(parseFHIRDateTime('2020-10').format(FHIRDateTimeFormat)).toEqual('2020-10-01T03:00:00Z');
-            expect(parseFHIRDateTime('2020-10-05').format(FHIRDateTimeFormat)).toEqual('2020-10-05T03:00:00Z');
-            expect(parseFHIRDateTime('2020-10-05 12').format(FHIRDateTimeFormat)).toEqual('2020-10-05T15:00:00Z');
-            expect(parseFHIRDateTime('2020-10-05 13:30').format(FHIRDateTimeFormat)).toEqual('2020-10-05T16:30:00Z');
-            expect(parseFHIRDateTime('2020-10-05 20:00:00').format(FHIRDateTimeFormat)).toEqual('2020-10-05T23:00:00Z');
-            expect(parseFHIRDateTime('2020-10-05 20:00:00').format(FHIRDateTimeFormat)).toEqual('2020-10-05T23:00:00Z');
+            expect(parseFHIRDateTime('2020').format(FHIRDateTimeFormat)).toEqual('2020-01-01T09:00:00Z');
+            expect(parseFHIRDateTime('2020-10').format(FHIRDateTimeFormat)).toEqual('2020-10-01T09:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05').format(FHIRDateTimeFormat)).toEqual('2020-10-05T09:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05 12').format(FHIRDateTimeFormat)).toEqual('2020-10-05T21:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05 13:30').format(FHIRDateTimeFormat)).toEqual('2020-10-05T22:30:00Z');
+            expect(parseFHIRDateTime('2020-10-05 20:00:00').format(FHIRDateTimeFormat)).toEqual('2020-10-06T05:00:00Z');
+            expect(parseFHIRDateTime('2020-10-05 20:00:00').format(FHIRDateTimeFormat)).toEqual('2020-10-06T05:00:00Z');
         });
     });
 
