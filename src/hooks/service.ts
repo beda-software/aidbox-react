48197f3589777462dc12c768eaab6f064b44d66c
Vadim Laletin <rusmiligamer@gmail.com>
2022-02-17 18:23:32 +0300

    Add updated version of reload/load

--- src/hooks/service.ts
+++ src/hooks/service.ts
@@ -1,38 +1,58 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import { loading, notAsked, RemoteData, RemoteDataResult, success, failure, isSuccess } from '../libs/remoteData';
 
-export interface ServiceManager<S> {
-    reload: () => void;
+export interface ServiceManager<S, F> {
+    reload: () => Promise<RemoteDataResult<S, F>>;
+    softReload: () => Promise<RemoteDataResult<S, F>>;
     set: (dataOrFn: S | ((data: S) => S)) => void;
 }
 
 export function useService<S = any, F = any>(
     asyncFunction: () => Promise<RemoteDataResult<S, F>>,
     deps: ReadonlyArray<any> = []
-): [RemoteData<S, F>, ServiceManager<S>] {
+): [RemoteData<S, F>, ServiceManager<S, F>] {
     const [remoteData, setRemoteData] = useState<RemoteData<S, F>>(notAsked);
 
-    // Use another state variable to trigger effect on demand
-    const [reloadsCount, setReloadsCount] = useState(0);
+    const load = useCallback(async () => {
+        try {
+            return await asyncFunction();
+        } catch (err) {
+            return failure(err.response ? err.response.data : err.message);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, deps);
+
+    const softReload = useCallback(async () => {
+        const response = await load();
+        setRemoteData(response);
+
+        return response;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [...deps, load]);
+
+    const reload = useCallback(async () => {
+        setRemoteData(loading);
+        const response = await load();
+        setRemoteData(response);
+
+        return response;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [...deps, load]);
 
     useEffect(() => {
         (async () => {
             setRemoteData(loading);
-            try {
-                const response: RemoteDataResult<S, F> = await asyncFunction();
-                setRemoteData(response);
-            } catch (err: any) {
-                setRemoteData(failure(err.response ? err.response.data : err.message));
-            }
+            setRemoteData(await load());
         })();
-        // eslint-disable-next-line
-    }, deps.concat(reloadsCount));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [...deps, load]);
 
     return [
         remoteData,
         {
-            reload: () => setReloadsCount((count) => count + 1),
+            reload,
+            softReload,
             set: (dataOrFn: S | ((data: S) => S)) => {
                 if (typeof dataOrFn === 'function') {
                     setRemoteData((rd) => (isSuccess(rd) ? success((dataOrFn as (data: S) => S)(rd.data)) : rd));
